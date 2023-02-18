94f3055343587b8dbd16f1087e9fd1617cd9a34a
Lupita Reyes <lupita.g.reyes@outlook.com>
2023-02-17 19:42:58 -0500

    Refactor and add tests

---
+++ index.js
@@ -0,0 +1,12 @@
+// Users run this file to get breed description
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv.slice(2)[0];
+
+fetchBreedDescription(breedName, (error, description) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(description);
+  }
+});
